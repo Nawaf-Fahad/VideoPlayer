db3818177a87525e8a19eec4fbdab16190b615be
Nawaf <63883911+Nawaf-Fahad@users.noreply.github.com>
2022-07-05 18:51:26 +0300

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Nawaf Alotaibi on 05/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
