d40301b32510fc7889c2b283aad4ce5d28c60d08
Nawaf <63883911+Nawaf-Fahad@users.noreply.github.com>
2022-07-10 10:22:06 +0300

    all app reday

---
+++ VideoPlayer/Views/VideoView.swift
@@ -0,0 +1,29 @@
+//
+//  VideoView.swift
+//  VideoPlayer
+//
+//  Created by Nawaf Alotaibi on 05/07/2022.
+//
+
+import SwiftUI
+import AVKit
+struct VideoView: View {
+    var video :Video
+    @State private var player = AVPlayer()
+    var body: some View {
+       VideoPlayer(player: player)
+            .edgesIgnoringSafeArea(.all)
+            .onAppear(){
+                if let link = video.videoFiles.first?.link{
+                    player = AVPlayer(url:URL(string: link)!)
+                    player.play()
+                }
+            }
+    }
+}
+
+struct VideoView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoView(video: previewVideo)
+    }
+}
